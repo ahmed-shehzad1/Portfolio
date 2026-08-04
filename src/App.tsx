1b9d445cf5dbf95be891d349f68a62a8e1585d93
Ahmed Shahzad <243330@students.au.edu.pk>
2026-08-04 17:26:20 +0500

    Directory created

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <main className="relative min-h-screen w-full overflow-hidden bg-void text-white bg-grain antialiased selection:bg-refract-cyan/20">
+      {/* Future Canvas */}
+      {/* Future Cursor */}
+      {/* Future Sections */}
+    </main>
+  );
+}
